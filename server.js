11c1a0d7de37476e2cfb49534a1be55fec206f9b
Ewelina Nowak <ewel.nowak@gmail.com>
2022-05-31 08:05:13 +0200

    Form final version

--- server.js
+++ server.js
@@ -1,49 +1,65 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
-const hbs = require('express-handlebars');
-const { render } = require('express/lib/response');
 
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', (req, res) => {
-    res.render('index');
+const storage = multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, './public');
+	},
+	filename: function (req, file, callback) {
+		callback(null, file.originalname);
+	},
 });
 
-app.get('/home', (req, res) => {
-    res.render('index');
+var upload = multer({ storage: storage });
+
+app.post('/contact/send-message', upload.single('uploadFile'), (req, res) => {
+	const { author, sender, title, message } = req.body;
+
+	if (author && sender && title && req.file && message) {
+		res.render('contact', { isSent: true, fileName: req.file.originalname });
+	} else {
+		res.render('contact', { isError: true });
+	}
+});
+
+app.get('/hello/:name', (req, res) => {
+	res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
 app.get('/about', (req, res) => {
-   res.render('about', { layout: 'dark' });
+	res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+	res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-    res.render('info');
+	res.render('info');
 });
 
-app.get('/history', (req, res) => {
-    res.render('history');
-});
-
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.get('/hello/:name', (req, res) => {
-    res.render('hello', { layout: false, name: req.params.name });
+app.get('/history', (req, res, next) => {
+	res.render('history');
 });
 
 app.use((req, res) => {
-    res.status(404).send('404 not found...');
-})
+	res.status(404).send('404 not found...');
+});
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+	console.log('Server is running on port: 8000');
 });
